c344cc4a32c1d26a0ee0e2be0c1f42fc79731de0
DanielWong76 <danielwong0505@gmail.com>
2023-08-04 12:32:55 -0400

    Cleaned up imports

--- app/hooks/useFonts.ts
+++
@@ -1,9 +0,0 @@
-
-import * as Font from 'expo-font';
-
-const useFonts = async () =>
-  await Font.loadAsync({
-    MochiyPopOne: require('../assets/fonts/MochiyPopOne-Regular.ttf'),
-  });
-
-export default useFonts
