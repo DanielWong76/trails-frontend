178b3843960df1e9489abe654ff6cb198f233e75
DanielWong76 <danielwong0505@gmail.com>
2023-09-06 22:52:07 -0400

    Deleted redux

--- app/redux/actions/index.ts
+++
@@ -1,7 +0,0 @@
-import { Action } from 'redux';
-
-export interface ReduxAction<Payload = any, Error = any, Meta = any> extends Action {
-  payload?: Payload;
-  error?: Error;
-  meta?: Meta;
-}
--- app/redux/store/index.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Provider,useSelector,useDispatch } from 'react-redux';
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-
