41f21887f6e652579d79437613c0c2e1af3c9711
DanielWong76 <danielwong0505@gmail.com>
2023-09-16 13:59:17 -0400

    Added frontend functionality for uploading

--- app/pages/OnboardingPage/index.tsx
+++ app/pages/OnboardingPage/index.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import Pages from '../../constants/pages';
 import { Colors } from '../../constants/styles';
 import TrailsLogo from '../../components/trailsLogo';
-import { View, Text, ImageBackground, Image } from 'react-native';
+import { View, Text, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { CSS } from '../../constants/styles';
 import Button from '../../components/button';
+import * as ImagePicker from 'expo-image-picker';
 
 type Props = {
     styles?
   }
 
+
+
 const ImagePage: React.FC<Props> = (props: Props) => {
+    const [photo, setPhoto] = React.useState(null);
+
+    const handleChoosePhoto = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+          console.log(result);
+
+        if (!result.canceled) {
+            setPhoto(result.assets[0].uri);
+        }
+      };
+
     return(
         <View style={styles.background}>
             <ImageBackground source={require('../../assets/images/ai-photo.png')} style={styles.image}>
@@ -21,10 +40,12 @@ const ImagePage: React.FC<Props> = (props: Props) => {
                 <Text style={styles.title}>
                     Upload Image
                 </Text>
-                <Image source={require('../../assets/images/defaultpfp.png')} style={styles.uploadLogo}/>
-                <View style={styles.uploadContainer}>
-                    <Image source={require('../../assets/images/upload.png')} style={styles.uploadLogo}/>
-                </View>
+                <Image source={photo ? { uri: photo} : require('../../assets/images/defaultpfp.png')} style={styles.profilePhoto}/>
+                <TouchableOpacity onPress={handleChoosePhoto}>
+                    <View style={styles.uploadContainer}>
+                        <Image source={require('../../assets/images/upload.png')} style={styles.uploadLogo}/>
+                    </View>
+                </TouchableOpacity>
                 <Button label={'Continue'} url={Pages.landing} background={Colors.primaryDark} color={Colors.white}/>
                 <Button label={'Continue without Image'} url={Pages.landing} background={Colors.white} color={Colors.primaryDark}/>
           </View>
@@ -57,6 +78,12 @@ const styles = EStyleSheet.create({
     },
     uploadLogo: {
         alignSelf: 'center',
+    },
+    profilePhoto: {
+        width: 100,
+        height: 100,
+        alignSelf: 'center',
+        borderRadius: 30
     },
     name: {
       display: 'flex',
